545e4ab44d5d9b5aebbc4abe0dc5370aa2de274a
Mike Lasby <mklasby@gmail.com>
2024-07-16 17:31:36 -0600

    api for gmp

--- sparsimony/api.py
+++ sparsimony/api.py
@@ -1,15 +1,19 @@
+from typing import Optional
 import torch
 
 from sparsimony.distributions.base import (
+    BaseDistribution,
     UniformDistribution,
     ERKDistribution,
 )
 from sparsimony.schedulers.base import (
+    AcceleratedCubicScheduler,
     ConstantScheduler,
     CosineDecayScheduler,
 )
 from sparsimony.dst.rigl import RigL
 from sparsimony.dst.set import SET
+from sparsimony.dst.gmp import GMP
 
 
 def rigl(
@@ -79,4 +83,50 @@ def set(
         distribution=UniformDistribution(),
         optimizer=optimizer,
         sparsity=sparsity,
+    )
+
+
+def gmp(
+    optimizer: torch.optim.Optimizer,
+    t_accel: int,
+    t_end: int,
+    distribution: Optional[BaseDistribution] = None,
+    delta_t: int = 100,
+    initial_sparsity: float = 0.0,
+    accelerated_sparsity: float = 0.7,
+    final_sparsity: float = 0.9,
+):
+    """GMP* implementation by Kurtic et al.
+    https://proceedings.mlr.press/v234/kurtic24a.html
+
+    Args:
+        optimizer (torch.optim.Optimizer): Previously initialized optimizer for
+            training. Used to override the dense gradient buffers for
+            sparse weights.
+        t_accel (int): Step to jump to accelerated sparsity level
+        t_end (int): Step to stop pruning model
+        distribution (Optional[BaseDistribution], optional): Layerwise sparisty
+            distribution. If None, uses uniform. Defaults to None.
+        delta_t (int, optional): Steps between topology update. Defaults to 100.
+        initial_sparsity (float, optional): Defaults to 0.0.
+        accelerated_sparsity (float, optional): Sparsity to jump to at t_accel
+            step. Defaults to 0.7.
+        final_sparsity (float, optional): Final sparsity. Defaults to 0.9.
+
+    Returns:
+        GMP: GMP sparsifier
+    """
+    if distribution is None:
+        distribution = UniformDistribution()
+    return GMP(
+        scheduler=AcceleratedCubicScheduler(
+            t_end=t_end,
+            delta_t=delta_t,
+            t_accel=t_accel,
+            initial_sparsity=initial_sparsity,
+            accelerated_sparsity=accelerated_sparsity,
+            final_sparsity=final_sparsity,
+        ),
+        distribution=distribution,
+        optimizer=optimizer,
     )
