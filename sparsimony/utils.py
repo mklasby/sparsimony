a1f6ccbe8039fa5b965ec8f550846f481b0a1396
Mike Lasby <mklasby@gmail.com>
2024-07-22 13:28:02 -0600

    sharing func added to utils

--- sparsimony/utils.py
+++ sparsimony/utils.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 
+from sparsimony.parametrization.fake_sparsity import (
+    FakeSparsity,
+    FakeSparsityDenseGradBuffer,
+)
+
 
 def get_mask(
     module: nn.Module, tensor_name: str = "weight", param_idx: int = 0, **kwargs
@@ -30,3 +35,30 @@ def path_length(history: torch.Tensor):
     for i in range(len(history) - 1):
         path_length += torch.abs(history[i] - history[i + 1])
     return path_length
+
+
+def share_parametrizations(
+    primary: nn.Module,
+    replica: nn.Module,
+    tensor_name: str = "weight",
+    param_idx: int = 0,
+):
+    __SUPPORTED_PARAMETRIZATIONS = [FakeSparsity, FakeSparsityDenseGradBuffer]
+    if not hasattr(primary, "parametrizations") or not hasattr(
+        replica, "parametrizations"
+    ):
+        raise ValueError("Primary and replica modules must be parametrized!")
+    primary_para = get_parametrization(primary, tensor_name, param_idx)
+    replica_para = get_parametrization(replica, tensor_name, param_idx)
+    if type(primary_para) not in __SUPPORTED_PARAMETRIZATIONS:
+        raise ValueError(
+            f"{type(primary_para)} is not supported for sharing."
+            f"Use one of: {__SUPPORTED_PARAMETRIZATIONS}"
+        )
+    if type(primary_para) is not type(replica_para):
+        raise ValueError(
+            f"Primary is parametrized with type {type(primary_para)} but "
+            f"replica is parametrized with type {type(replica_para)}"
+        )
+    for name, _ in primary_para.named_buffers():
+        setattr(replica, name, getattr(primary, name))
