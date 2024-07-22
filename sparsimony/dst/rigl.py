eb37663351776f82bf8f0d6f9b8149df1fb2d2de
Mike Lasby <mklasby@gmail.com>
2024-07-22 14:02:07 -0600

    fixed import of parametrization in rigl

--- sparsimony/dst/rigl.py
+++ sparsimony/dst/rigl.py
@@ -1,5 +1,4 @@
 from typing import Optional, Dict, Any
-from sparsimony import parametrization
 import torch
 import torch.nn as nn
 from torch.ao.pruning.sparsifier.base_sparsifier import BaseSparsifier
