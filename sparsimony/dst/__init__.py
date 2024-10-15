a30a59e7eb9aa818b98ed013d21596785cb3d5b6
Mike Lasby <mklasby@gmail.com>
2024-10-15 12:41:57 -0600

    nmSRigL implemented

--- sparsimony/dst/__init__.py
+++ sparsimony/dst/__init__.py
@@ -2,6 +2,6 @@ from .dense import DenseNoOp
 from .gmp import GMP
 from .rigl import RigL
 from .set import SET
+from .srigl import SRigL, NMSRigL
 
-# from .srigl import SRigL, SRigLTwoFour
 # from .static import StaticMagnitudeSparsifier
