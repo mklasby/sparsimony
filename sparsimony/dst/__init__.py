d2ae1c84cac0d2a6fdea0215a73c8acbce99968f
Mike Lasby <mklasby@gmail.com>
2025-02-05 11:30:55 -0700

    updated init for dst

--- sparsimony/dst/__init__.py
+++ sparsimony/dst/__init__.py
@@ -3,5 +3,4 @@ from .gmp import GMP
 from .rigl import RigL
 from .set import SET
 from .srigl import SRigL, NMSRigL
-
-# from .static import StaticMagnitudeSparsifier
+from .static import StaticMagnitudeSparsifier, StaticSparsifier
